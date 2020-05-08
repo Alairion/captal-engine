e6441d545410728158251b158af31be25f541790
Alairion <alexy.pellegrini@laposte.net>
2020-05-09 01:51:10 +0200

    -

--- captal/src/zlib.cpp
+++ captal/src/zlib.cpp
@@ -7,16 +7,19 @@
 namespace cpt
 {
 
-zlib_compressor::zlib_compressor(std::uint32_t compression_level)
+namespace impl
+{
+
+deflate::deflate(std::uint32_t compression_level, std::int32_t window_bits)
 :m_stream{std::make_unique<z_stream_s>()}
 {
     assert(compression_level <= 9 && "compression level must be in range [0; 9]");
 
-    if(deflateInit(m_stream.get(), static_cast<int>(compression_level)) != Z_OK)
+    if(deflateInit2(m_stream.get(), static_cast<int>(compression_level), Z_DEFLATED, static_cast<int>(window_bits), 8, Z_DEFAULT_STRATEGY) != Z_OK)
         throw std::runtime_error{"Can not init deflate stream."};
 }
 
-zlib_compressor::~zlib_compressor()
+deflate::~deflate()
 {
     if(m_stream)
     {
@@ -24,23 +27,27 @@ zlib_compressor::~zlib_compressor()
     }
 }
 
-void zlib_compressor::compress_impl(const std::uint8_t*& input, std::size_t input_size, std::uint8_t*& output, std::size_t output_size, bool finish)
+bool deflate::compress_impl(const std::uint8_t*& input, std::size_t input_size, std::uint8_t*& output, std::size_t output_size, bool finish)
 {
     m_stream->next_in = reinterpret_cast<const Bytef*>(input);
     m_stream->avail_in = static_cast<uInt>(input_size);
     m_stream->next_out = reinterpret_cast<Bytef*>(output);
     m_stream->avail_out = static_cast<uInt>(output_size);
 
-    if(deflate(m_stream.get(), finish ? Z_FINISH : Z_NO_FLUSH) == Z_STREAM_ERROR)
-        throw std::runtime_error{zError(Z_STREAM_ERROR)};
+    if(::deflate(m_stream.get(), finish ? Z_FINISH : Z_NO_FLUSH) == Z_STREAM_ERROR)
+        return false;
 
     input = reinterpret_cast<const std::uint8_t*>(m_stream->next_in);
     output = reinterpret_cast<std::uint8_t*>(m_stream->next_out);
+
+    return true;
 }
 
-std::size_t zlib_compressor::compress_bound(std::size_t input_size) const noexcept
+std::size_t deflate::compress_bound(std::size_t input_size) const noexcept
 {
-    return static_cast<std::size_t>(compressBound(static_cast<uLong>(input_size)));
+    return static_cast<std::size_t>(deflateBound(m_stream.get(), static_cast<uLong>(input_size)));
+}
+
 }
 
 }
--- captal/src/zlib.hpp
+++ captal/src/zlib.hpp
@@ -12,24 +12,30 @@ struct z_stream_s;
 namespace cpt
 {
 
-class zlib_compressor
+namespace impl
+{
+
+class deflate
 {
 public:
+    static constexpr std::uint32_t default_compression{6};
     static constexpr bool flush{true};
+    static constexpr bool known_compress_bound{true};
 
 public:
-    zlib_compressor(std::uint32_t compression_level = 6);
-    ~zlib_compressor();
-    zlib_compressor(const zlib_compressor&) = delete;
-    zlib_compressor& operator=(const zlib_compressor&) = delete;
-    zlib_compressor(zlib_compressor&&) noexcept = default;
-    zlib_compressor& operator=(zlib_compressor&&) noexcept = default;
+    constexpr deflate() noexcept = default;
+    deflate(std::uint32_t compression_level, std::int32_t window_bits);
+    ~deflate();
+    deflate(const deflate&) = delete;
+    deflate& operator=(const deflate&) = delete;
+    deflate(deflate&&) noexcept = default;
+    deflate& operator=(deflate&&) noexcept = default;
 
     template<typename InContiguousIt, typename OutContiguousIt>
-    void compress(InContiguousIt& input_begin, InContiguousIt input_end, OutContiguousIt& output_begin, OutContiguousIt output_end, bool flush = false)
+    bool compress(InContiguousIt& input_begin, InContiguousIt input_end, OutContiguousIt& output_begin, OutContiguousIt output_end, bool flush = false)
     {
-        static_assert(sizeof(typename std::iterator_traits<InContiguousIt>::value_type) == 1, "cpt::zlib_compressor::compress only works on bytes.");
-        static_assert(sizeof(typename std::iterator_traits<OutContiguousIt>::value_type) == 1, "cpt::zlib_compressor::compress only works on bytes.");
+        static_assert(sizeof(typename std::iterator_traits<InContiguousIt>::value_type) == 1, "cpt::deflate::compress only works on bytes.");
+        static_assert(sizeof(typename std::iterator_traits<OutContiguousIt>::value_type) == 1, "cpt::deflate::compress only works on bytes.");
 
         const std::uint8_t* const input_address{reinterpret_cast<const std::uint8_t*>(std::addressof(*input_begin))};
         const std::uint8_t* input_ptr{input_address};
@@ -39,10 +45,13 @@ public:
         std::uint8_t* output_ptr{output_address};
         const std::size_t output_size{static_cast<std::size_t>(std::distance(output_begin, output_end))};
 
-        compress_impl(input_ptr, input_size, output_ptr, output_size, flush);
+        if(!compress_impl(input_ptr, input_size, output_ptr, output_size, flush))
+            return false;
 
         std::advance(input_begin, input_ptr - input_address);
         std::advance(output_begin, output_ptr - output_address);
+
+        return true;
     }
 
     std::size_t compress_bound(std::size_t input_size) const noexcept;
@@ -50,17 +59,73 @@ public:
     template<typename InputIt>
     std::size_t compress_bound(InputIt begin, InputIt end) const
     {
-        return compress_bound(static_cast<std::size_t>(std::distance(begin, end) * sizeof(typename std::iterator_traits<InputIt>::value_type)));
+        static_assert(sizeof(typename std::iterator_traits<InputIt>::value_type) == 1, "cpt::deflate::compress only works on bytes.");
+
+        return compress_bound(static_cast<std::size_t>(std::distance(begin, end)));
     }
 
 private:
-    void compress_impl(const std::uint8_t*& input, std::size_t input_size, std::uint8_t*& output, std::size_t output_size, bool finish);
+    bool compress_impl(const std::uint8_t*& input, std::size_t input_size, std::uint8_t*& output, std::size_t output_size, bool finish);
 
 private:
     std::unique_ptr<z_stream_s> m_stream{};
 };
 
+}
 
+class deflate : public impl::deflate
+{
+public:
+    constexpr deflate() noexcept = default;
+
+    deflate(std::uint32_t compression_level)
+    :impl::deflate{compression_level, -15}
+    {
+
+    }
+
+    ~deflate() = default;
+    deflate(const deflate&) = delete;
+    deflate& operator=(const deflate&) = delete;
+    deflate(deflate&&) noexcept = default;
+    deflate& operator=(deflate&&) noexcept = default;
+};
+
+class zlib_deflate : public impl::deflate
+{
+public:
+    constexpr zlib_deflate() noexcept = default;
+
+    zlib_deflate(std::uint32_t compression_level)
+    :impl::deflate{compression_level, 15}
+    {
+
+    }
+
+    ~zlib_deflate() = default;
+    zlib_deflate(const zlib_deflate&) = delete;
+    zlib_deflate& operator=(const zlib_deflate&) = delete;
+    zlib_deflate(zlib_deflate&&) noexcept = default;
+    zlib_deflate& operator=(zlib_deflate&&) noexcept = default;
+};
+
+class gzip_deflate : public impl::deflate
+{
+public:
+    constexpr gzip_deflate() noexcept = default;
+
+    gzip_deflate(std::uint32_t compression_level)
+    :impl::deflate{compression_level, 15 + 16}
+    {
+
+    }
+
+    ~gzip_deflate() = default;
+    gzip_deflate(const gzip_deflate&) = delete;
+    gzip_deflate& operator=(const gzip_deflate&) = delete;
+    gzip_deflate(gzip_deflate&&) noexcept = default;
+    gzip_deflate& operator=(gzip_deflate&&) noexcept = default;
+};
 
 }
 
