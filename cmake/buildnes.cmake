429b3ab956511f918570e8c3294eff662c6e6603
Alairion <alexy.pellegrini@laposte.net>
2020-09-27 22:31:26 +0200

    -

--- cmake/buildnes.cmake
+++ cmake/buildnes.cmake
@@ -1,5 +1,6 @@
 set(NES_BUILD_EXAMPLES OFF CACHE INTERNAL "")
 
+captal_download_submodule(external/nes)
 add_subdirectory(external/nes external/nes/bin EXCLUDE_FROM_ALL)
 
 unset(NES_BUILD_EXAMPLES CACHE)
--- cmake/download_submodule.cmake
+++ cmake/download_submodule.cmake
@@ -1,24 +1,33 @@
+set(CAPTAL_ROOT_DIRECTORY ${PROJECT_SOURCE_DIR})
+
+find_package(Git QUIET)
+
 function(captal_download_submodule PATH)
 
-    find_package(Git QUIET)
-
-    if(GIT_FOUND)
-        message(STATUS "Downloading submodule ${PATH}")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init ${PATH}
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
-
-        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
-        endif()
+    if(EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
+        message(STATUS "Submodule ${PATH} already downloaded")
+        return()
     endif()
 
-    if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${PATH}/CMakeLists.txt")
-        if(NOT GIT_FOUND)
-            message(FATAL_ERROR "Git is not installed on this device, and is required to download submodules.")
+    if(NOT GIT_FOUND)
+        message(FATAL_ERROR "Git is not installed on this device, and is required to download submodules.")
+    endif()
+
+    message(STATUS "Downloading submodule ${PATH}")
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init ${PATH}
+                    WORKING_DIRECTORY ${CAPTAL_ROOT_DIRECTORY}
+                    RESULT_VARIABLE GIT_SUBMOD_RESULT)
+
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+        message(FATAL_ERROR "\"git submodule update --init ${PATH}\" failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+    endif()
+
+    if(NOT EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
         else()
             message(FATAL_ERROR "The submodule \"${PATH}\" has not been downloaded! Try update submodules and try again.")
         endif()
     endif()
+
+    message(STATUS "Successfully downloaded submodule ${PATH}")
 
 endfunction()
