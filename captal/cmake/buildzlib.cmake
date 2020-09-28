7337a0733808c4af602e720ee95c91ba2d826e3f
Alairion <alexy.pellegrini@laposte.net>
2020-09-28 15:30:59 +0200

    -

--- captal/cmake/buildzlib.cmake
+++ captal/cmake/buildzlib.cmake
@@ -1,4 +1,7 @@
 
 captal_download_submodule(captal/external/zlib TRUE)
-file(COPY cmake/zlib/CMakeLists.txt DESTINATION external/zlib)
+
+file(REMOVE external/zlib/CMakeLists.txt)
+file(COPY ${PROJECT_SOURCE_DIR}/cmake/zlib/CMakeLists.txt DESTINATION ${PROJECT_SOURCE_DIR}/external/zlib)
+
 add_subdirectory(external/zlib EXCLUDE_FROM_ALL)
