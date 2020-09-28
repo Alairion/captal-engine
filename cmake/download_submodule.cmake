0d22b13943c119d55c584aacb6536505f4217a60
Alairion <alexy.pellegrini@laposte.net>
2020-09-28 11:07:47 +0200

    -

--- cmake/download_submodule.cmake
+++ cmake/download_submodule.cmake
@@ -4,11 +4,9 @@ find_package(Git QUIET)
 
 function(captal_download_submodule PATH CHECK_CMAKEFILES)
 
-    if(CHECK_CMAKEFILES)
-        if(EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
-            message(STATUS "Submodule ${PATH} already downloaded")
-            return()
-        endif()
+    if(CHECK_CMAKEFILES AND EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
+        message(STATUS "Submodule ${PATH} already downloaded")
+        return()
     endif()
 
     if(NOT GIT_FOUND)
@@ -24,10 +22,8 @@ function(captal_download_submodule PATH CHECK_CMAKEFILES)
         message(FATAL_ERROR "\"git submodule update --init ${PATH}\" failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
     endif()
 
-    if(CHECK_CMAKEFILES)
-        if(NOT EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
-            message(FATAL_ERROR "The submodule \"${PATH}\" has not been downloaded! Try update submodules and try again.")
-        endif()
+    if(CHECK_CMAKEFILES AND NOT EXISTS "${CAPTAL_ROOT_DIRECTORY}/${PATH}/CMakeLists.txt")
+        message(FATAL_ERROR "The submodule \"${PATH}\" has not been downloaded! Try update submodules and try again.")
     endif()
 
     message(STATUS "Successfully downloaded submodule ${PATH}")
