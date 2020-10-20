cda61aeb29f5205d04cc3a26523963d0c701dec3
Alairion <alexy.pellegrini@laposte.net>
2020-10-21 01:02:10 +0200

    -

---
+++ captal/src/captal/bin_packing.cpp
@@ -0,0 +1,131 @@
+#include "bin_packing.hpp"
+
+namespace cpt
+{
+
+bin_packer::bin_packer(uint32_t width, uint32_t height)
+:m_width{width}
+,m_height{height}
+,m_spaces{rect{0, 0, width, height}}
+{
+    m_spaces.reserve(256);
+}
+
+std::optional<bin_packer::rect> bin_packer::append(uint32_t image_width, uint32_t image_height)
+{
+    for(auto i{std::ssize(m_spaces) - 1}; i >= 0; --i)
+    {
+        const auto candidate{m_spaces[i]};
+        const auto splits{split(image_width, image_height, candidate)};
+
+        if(splits)
+        {
+            m_spaces.erase(std::begin(m_spaces) + i);
+
+            for(std::size_t j{}; j < splits->count; ++j)
+            {
+                m_spaces.emplace_back(splits->splits[j]);
+            }
+
+            return rect{candidate.x, candidate.y, image_width, image_height};
+        }
+    }
+
+    return std::nullopt;
+}
+
+void bin_packer::resize(uint32_t width, uint32_t height) noexcept
+{
+    for(auto& space : m_spaces)
+    {
+        if(space.x + space.width == m_width)
+        {
+            space.width = width - space.x;
+        }
+
+        if(space.y + space.height == m_height)
+        {
+            space.height = height - space.y;
+        }
+    }
+
+    m_width = width;
+    m_height = height;
+}
+
+std::optional<bin_packer::splits> bin_packer::split(uint32_t image_width, uint32_t image_height, const bin_packer::rect& space) noexcept
+{
+    const auto free_width{static_cast<std::int32_t>(space.width) - static_cast<std::int32_t>(image_width)};
+    const auto free_height{static_cast<std::int32_t>(space.height) - static_cast<std::int32_t>(image_height)};
+
+    if(free_width < 0 || free_height < 0)
+    {
+        return std::nullopt;
+    }
+
+    if(free_width == 0 && free_height == 0)
+    {
+        return splits{};
+    }
+
+    if(free_width > 0 && free_height == 0)
+    {
+        auto output{space};
+
+        output.x += image_width;
+        output.width -= image_width;
+
+        return splits{1, {output}};
+    }
+
+    if(free_width == 0 && free_height > 0)
+    {
+        auto output{space};
+
+        output.y += image_height;
+        output.height -= image_height;
+
+        return splits{1, {output}};
+    }
+
+    if(free_width > free_height)
+    {
+        const rect bigger_split
+        {
+            space.x + image_width,
+            space.y,
+            static_cast<std::uint32_t>(free_width),
+            space.height
+        };
+
+        const rect lesser_split
+        {
+            space.x,
+            space.y + image_height,
+            image_width,
+            static_cast<std::uint32_t>(free_height)
+        };
+
+        return splits{2, {bigger_split, lesser_split}};
+    }
+
+    const rect bigger_split
+    {
+        space.x,
+        space.y + image_height,
+        space.width,
+        static_cast<std::uint32_t>(free_height)
+    };
+
+    const rect lesser_split
+    {
+        space.x + image_width,
+        space.y,
+        static_cast<std::uint32_t>(free_width),
+        image_height
+    };
+
+    return splits{2, {bigger_split, lesser_split}};
+}
+
+}
---
+++ captal/src/captal/bin_packing.hpp
@@ -0,0 +1,64 @@
+#ifndef CAPTAL_BIN_PACKING_HPP_INCLUDED
+#define CAPTAL_BIN_PACKING_HPP_INCLUDED
+
+#include "config.hpp"
+
+#include <vector>
+#include <array>
+#include <optional>
+
+namespace cpt
+{
+
+class bin_packer
+{
+public:
+    struct rect
+    {
+        std::uint32_t x{};
+        std::uint32_t y{};
+        std::uint32_t width{};
+        std::uint32_t height{};
+    };
+
+public:
+    bin_packer() = default;
+    bin_packer(std::uint32_t width, std::uint32_t height);
+
+    bin_packer(const bin_packer&) = delete;
+    bin_packer& operator=(const bin_packer&) = delete;
+    bin_packer(bin_packer&&) noexcept = default;
+    bin_packer& operator=(bin_packer&&) noexcept = default;
+
+    std::optional<rect> append(std::uint32_t image_width, std::uint32_t image_height);
+    void resize(std::uint32_t width, std::uint32_t height) noexcept;
+
+    std::uint32_t width() const noexcept
+    {
+        return m_width;
+    }
+
+    std::uint32_t height() const noexcept
+    {
+        return m_height;
+    }
+
+private:
+    struct splits
+    {
+        std::size_t count{};
+        std::array<rect, 2> splits{};
+    };
+
+private:
+    std::optional<splits> split(std::uint32_t image_width, std::uint32_t image_height, const rect& space) noexcept;
+
+private:
+    std::uint32_t m_width{};
+    std::uint32_t m_height{};
+    std::vector<rect> m_spaces{};
+};
+
+}
+
+#endif
