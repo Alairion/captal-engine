b21fc2674835f4fc706e85a85f76464da7e4238d
Alairion <alexy.pellegrini@laposte.net>
2020-11-07 20:45:35 +0100

    -

--- captal/text.cpp
+++ captal/text.cpp
@@ -1,7 +1,6 @@
 #include <array>
 
 #include <captal/engine.hpp>
-#include <captal/entity.hpp>
 
 #include <captal/components/camera.hpp>
 #include <captal/components/drawable.hpp>
@@ -15,9 +14,13 @@
 
 #include "sansation.hpp"
 
-static void setup(cpt::registry& world)
+static void setup(entt::registry& world)
 {
+    cpt::text_drawer drawer{cpt::font{sansation_regular_font_data, 16}};
 
+    const auto text{world.create()};
+    world.emplace<cpt::components::node>(text);
+    world.emplace<cpt::components::drawable>(text, drawer.draw("Hello world!", cpt::text_style::bold, cpt::colors::black));
 }
 
 static void run()
@@ -25,7 +28,7 @@ static void run()
     auto window{cpt::make_render_window("Captal test", cpt::video_mode{1280, 800}, apr::window_options::resizable)};
     window->set_clear_color(cpt::colors::white);
 
-    cpt::registry world{};
+    entt::registry world{};
 
     const auto camera{world.create()};
     world.emplace<cpt::components::node>(camera);
@@ -62,32 +65,17 @@ int main()
 
         const cpt::audio_parameters audio
         {
-            //The number of channels, 2 is stereo.
             .channel_count = 2,
-            //The frequency of the output stream.
             .frequency = 44100
         };
 
         const cpt::graphics_parameters graphics
         {
-            //The renderer options (c.f. tph::renderer_options)
             .options = tph::renderer_options::tiny_memory_heaps,
-            //The physical device's features. Real application must check feature availability
-            .features = tph::physical_device_features
-            {
-                //Enable sample shading (i.e. MSAA inside textures)
-                .sample_shading = true
-            }
         };
 
-        //The engine instance. It must be created before most call to captal functions.
-        //The first value is your application name. It will be passed to Tephra's instance then to Vulkan's instance.
-        //The second value is your application version. It will be passed to Tephra's instance then to Vulkan's instance.
-        //Then give the underlying system, audio and graphics parameters.
-        //If you don't need anything special, there is a constructor without these additionnal parameters
         cpt::engine engine{"captal_test", cpt::version{0, 1, 0}, system, audio, graphics};
 
-        //The engine is reachable by its static address. We don't need to keep a reference on it.
         run();
     }
     catch(const std::exception& e)
